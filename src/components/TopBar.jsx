bd61b750912b37432b43db43f7e2e4a5a6cc2359
jssmith79 <i.am.hef@gmail.com>
2023-04-20 21:22:27 -0700

    completed activity

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,24 @@
+import instacartLogoSvg from '../logo.svg'
+import { Navbar, Button, Nav } from "react-bootstrap"
+
+
+export default function TopBar() {
+    return (<Navbar bg="light" expand='lg' fixed='top'>
+        <Navbar.Brand
+            style={
+                { margin: '5px 0 0 20px' }
+            }
+            href='#home'
+        >
+            <img src={instacartLogoSvg} alt='Logo Image' />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto'></Nav>
+            <Nav>
+                <Nav.Link href='#login'>Login</Nav.Link>
+                <Button variant='succcess'>Sign Up</Button>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>)
+}
